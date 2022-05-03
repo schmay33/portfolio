067988a904695befff1887db4ed03f0ff0de778e
Nathan Schmidt <nschmdt@gmail.com>
2022-05-03 11:35:08 -0500

    Added preloader

--- js/main.js
+++ js/main.js
@@ -2,10 +2,15 @@ const arrowLinks = document.getElementsByClassName("arrow-link");
 
 //loop through all the arrow links and apply on hover event
 for (let i = 0; i < arrowLinks.length; i++) {
-  arrowLinks[i].addEventListener("mouseover", function() {
-    //this.style.color = "#ffc107";
-  });
-  arrowLinks[i].addEventListener("mouseout", function() {
-    //this.style.color = "inherit";
-  });
+	arrowLinks[i].addEventListener("mouseover", function () {
+		//this.style.color = "#ffc107";
+	});
+	arrowLinks[i].addEventListener("mouseout", function () {
+		//this.style.color = "inherit";
+	});
 }
+
+const preloader = document.getElementById("preloader");
+window.addEventListener('load', function () {
+    preloader.style.display = "none";   
+});
