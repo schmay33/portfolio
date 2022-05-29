1af2efdb024725ac6ca3f82b8f3ac5a5e105f1e5
Schmay33 <nschmdt@gmail.com>
2022-05-29 18:15:23 +0800

    Add scale animation

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
+const myImage = document.getElementById("myImage");
+
 window.addEventListener("load", async () => {
-    const preloader = document.querySelector(".preloader");
-    const homepage = document.querySelector("#homepage");
+	const preloader = document.querySelector(".preloader");
+	const homepage = document.querySelector("#homepage");
 	//after 2.5 seconds fade out the preloader
 	setTimeout(() => {
-        preloader.classList.add("preloader-fadeOut");
-        homepage.classList.add("fadeIn");
+		preloader.classList.add("preloader-fadeOut");
+		homepage.classList.add("fadeIn");
 	}, 2000);
+
+	myImage.addEventListener("mouseover", () => {
+		scaleImage(myImage);
+	});
 });
+
+function scaleImage(img) {
+    img.style.animation = "scale-animation 1s ease-in-out";
+}
