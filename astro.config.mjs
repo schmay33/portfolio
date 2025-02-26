3d81bd555b69ca628de3b74622e36a9158136edd
schmay33 <nschmdt@gmail.com>
2025-02-26 14:20:37 +0800

    Upate for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nschmdt.github.io',
+  site: 'https://schmay33.github.io',
   base: '/portfolio',
   integrations: [
     react(),
